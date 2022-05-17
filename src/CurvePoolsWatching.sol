93cb9e683b96b68bbd829a0e286fa1ee004e2bab
Arthurim <arthurbdauphine@gmail.com>
2022-05-17 14:30:34 +0100

    try fetching balance

--- src/CurvePoolsWatching.sol
+++ src/CurvePoolsWatching.sol
@@ -1,38 +1,50 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.13;
 
-abstract contract CurvePoolInterface {
-    function get_virtual_price() virtual external returns (uint256);
-}
-
-interface IStableSwapBUSD {
+interface IStableSwap {
+    function balances(uint i) external view returns (uint);
     function get_virtual_price() external returns (uint256);
 }
 
 contract PriceAndSlippageComputerContract {
     address owner;
     string ownerName;
-    IStableSwapBUSD public stableSwapBUSD;
+    IStableSwap public stableSwap;
+    // for each AMM there are different pairs
+    mapping (string => mapping (string => address)) public pools;
+
     constructor(string memory _name){
         owner=msg.sender;
         ownerName=_name;
+        pools["Curve"]["BUSD"] = 0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27;
     }
     
+    function getBalance(uint ind) public view returns(uint256){
+        return stableSwap.balances(ind);
+    }
+
     function getOwnerName() public view returns(string memory) {
         return ownerName;
     }
 
 
-    function setCurvePoolContractAddress(address _address) external {
-        stableSwapBUSD = IStableSwapBUSD(_address);
+    function setCurvePoolContractAddress(address _address) public {
+        stableSwap = IStableSwap(_address);
     }
 
     function getCurvePoolContractAddress() public view returns(address) {
-        return address(stableSwapBUSD);
+        return address(stableSwap);
     }
 
     function getPrice() public returns(uint256){
-        return stableSwapBUSD.get_virtual_price();
+        return stableSwap.get_virtual_price();
+    }
+
+    function getPriceForPoolAndPair(string memory pool, string memory pair) public returns(uint256) {
+        require((keccak256(abi.encodePacked(pool)))==keccak256("Curve"));
+        address poolAddress = pools[pool][pair];
+        setCurvePoolContractAddress(poolAddress);
+        return stableSwap.get_virtual_price();
     }
 
 }
--- test/CurvePoolsWatching.t.sol
+++ test/CurvePoolsWatching.t.sol
@@ -8,7 +8,7 @@ import "../src/CurvePoolsWatching.sol";
 
 contract CurvePoolsWatchingTest is Test {
     PriceAndSlippageComputerContract priceAndSlippageComputer;
-    address curvePoolAddress = 0x3B3Ac5386837Dc563660FB6a0937DFAa5924333B;
+    address curvePoolAddress = 0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27;
     
     function setUp() public {
         priceAndSlippageComputer = new PriceAndSlippageComputerContract("Arthur");
@@ -27,6 +27,19 @@ contract CurvePoolsWatchingTest is Test {
     function testCanGetPrice() public {
         priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
         uint256 p = priceAndSlippageComputer.getPrice();
+        console.log("price", p);
+        assertTrue(p>0);
+    }
+
+    function testGetBalances() public {
+        priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
+        uint256 b = priceAndSlippageComputer.getBalance(0);
+        console.log("balance:", b);
+        assert(b>0);
+    }
+    function testGetPriceForPoolAndPair() public {
+        uint256 p = priceAndSlippageComputer.getPriceForPoolAndPair("Curve","BUSD");
+        console.log("price", p);
         assertTrue(p>0);
     }
 
