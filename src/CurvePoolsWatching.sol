f0a3cc09d8e7bd7c1e9cba20b5b98252a0c51c48
Arthurim <arthurbdauphine@gmail.com>
2022-05-17 15:51:30 +0100

    fetch balance o/

--- src/CurvePoolsWatching.sol
+++ src/CurvePoolsWatching.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.13;
 
 interface IStableSwap {
-    function balances(uint i) external view returns (uint);
+    function coins(int128 i) external view returns (address);
+    function underlying_coins(int128 i) external view returns (address);
+    function balances(int128 i) external view returns (uint);
     function get_virtual_price() external returns (uint256);
 }
 
@@ -12,15 +14,50 @@ contract PriceAndSlippageComputerContract {
     IStableSwap public stableSwap;
     // for each AMM there are different pairs
     mapping (string => mapping (string => address)) public pools;
+    // map token to their addresses
+    mapping (string => address) public tokens;
+    int N_COINS;
 
     constructor(string memory _name){
         owner=msg.sender;
         ownerName=_name;
         pools["Curve"]["BUSD"] = 0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27;
+        setUpTokens();
+    }
+
+    function setUpTokens() public {
+        tokens["DAI"] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+        tokens["USDC"] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        tokens["USDT"] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+        tokens["BUSD"] = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+        N_COINS = 4;
     }
     
-    function getBalance(uint ind) public view returns(uint256){
+    function getIndexOfToken(string memory token) public view returns (int128) {
+        address tokenAddress = tokens[token];
+        for(int128 i=0;i<N_COINS;++i){
+            if(tokenAddress==stableSwap.underlying_coins(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function getBalanceOfToken(string memory token) public view returns(uint){
+        return stableSwap.balances(getIndexOfToken(token));
+
+    }
+
+    function getBalance(int128 ind) public view returns(uint){
         return stableSwap.balances(ind);
+    }
+
+    function getCoin(int128 ind) public view returns(address){
+        return stableSwap.coins(ind);
+    }
+
+    function getUnderlyingCoin(int128 ind) public view returns(address){
+        return stableSwap.underlying_coins(ind);
     }
 
     function getOwnerName() public view returns(string memory) {
--- test/CurvePoolsWatching.t.sol
+++ test/CurvePoolsWatching.t.sol
@@ -31,12 +31,35 @@ contract CurvePoolsWatchingTest is Test {
         assertTrue(p>0);
     }
 
+    function testGetIndex() public {
+        priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
+        int daiIndex = priceAndSlippageComputer.getIndexOfToken("DAI");
+        assert(daiIndex == 0);
+    }
+
     function testGetBalances() public {
         priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
         uint256 b = priceAndSlippageComputer.getBalance(0);
         console.log("balance:", b);
         assert(b>0);
+        uint256 bDAI = priceAndSlippageComputer.getBalanceOfToken("DAI");
+        assert(b==bDAI);
     }
+
+    function testGetCoin() public {
+        priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
+        address b = priceAndSlippageComputer.getCoin(0);
+        console.log("coin:", b);
+        assert(1==1);
+    }
+
+    function testGetUnderlyingCoin() public {
+        priceAndSlippageComputer.setCurvePoolContractAddress(curvePoolAddress);
+        address b = priceAndSlippageComputer.getUnderlyingCoin(0);
+        console.log("ucoin:", b);
+        assert(1==1);
+    }
+
     function testGetPriceForPoolAndPair() public {
         uint256 p = priceAndSlippageComputer.getPriceForPoolAndPair("Curve","BUSD");
         console.log("price", p);
@@ -44,4 +67,7 @@ contract CurvePoolsWatchingTest is Test {
     }
 
 // forge test --fork-url http://rpcdaemon.erigon.dappnode:8545
+
+//underlying coin 0x6b175474e89094c44da98b954eedeac495271d0f
+//coin 0xc2cb1040220768554cf699b0d863a3cd4324ce32
 }
